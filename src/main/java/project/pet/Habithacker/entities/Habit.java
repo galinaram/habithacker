2cf616c3d9f9f55121ac92a7fddb5b0df55ff46a
galina <galina.ramodina@gmail.com>
2025-09-19 11:39:53 +0300

    feat: create Habit entity with basic fields

---
+++ src/main/java/project/pet/Habithacker/entities/Habit.java
@@ -0,0 +1,21 @@
+package project.pet.Habithacker.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Habit {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column (nullable = false)
+    String name;
+    String description;
+    @Column (nullable = false)
+    String targetCount;
+}
